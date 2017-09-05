4908d0f1da7ee1792820606f11662aec664c9520
Priya Prabhakar <Priya.Prabhakar@LibertyMutual.com>
2017-09-05 15:07:19 -0700

    Initial project startup commit

---
+++ src/test/java/com/liberymutual/goforcode/penneForYourThoughts/PenneForYourThoughtsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liberymutual.goforcode.penneForYourThoughts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PenneForYourThoughtsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
