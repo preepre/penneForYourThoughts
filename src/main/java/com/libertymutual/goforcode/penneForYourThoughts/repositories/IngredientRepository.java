adb590891f50154921fa5da1897e34539c109b13
Tania Asim <tania.asim@libertymutual.com>
2017-09-06 13:01:47 -0700

    This includes a main class for PenneForYourThoughts.

--- src/main/java/com/libertymutual/goforcode/penneForYourThoughts/repositories/IngredientRepository.java
+++ src/main/java/com/libertymutual/goforcode/penneForYourThoughts/repositories/IngredientRepository.java
@@ -1,10 +1,11 @@
 package com.libertymutual.goforcode.penneForYourThoughts.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.libertymutual.goforcode.penneForYourThoughts.models.Ingredient;
 
-
+@Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
 }
--- src/main/java/com/libertymutual/goforcode/penneForYourThoughts/repositories/InstructionRepository.java
+++ src/main/java/com/libertymutual/goforcode/penneForYourThoughts/repositories/InstructionRepository.java
@@ -1,9 +1,11 @@
 package com.libertymutual.goforcode.penneForYourThoughts.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.libertymutual.goforcode.penneForYourThoughts.models.Instruction;
 
+@Repository
 public interface InstructionRepository extends JpaRepository<Instruction, Long> {
 
 }
