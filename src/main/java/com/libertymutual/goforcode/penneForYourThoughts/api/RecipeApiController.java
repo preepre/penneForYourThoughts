95102763c654831a474102a61635bfbd08c91e20
Tania Asim <tania.asim@libertymutual.com>
2017-09-06 14:45:44 -0700

    Added ability to add a recipe.

--- src/main/java/com/libertymutual/goforcode/penneForYourThoughts/api/RecipeApiController.java
+++ src/main/java/com/libertymutual/goforcode/penneForYourThoughts/api/RecipeApiController.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,10 @@ public class RecipeApiController {
 
 	}
 	
+	@PostMapping("") // requestbody will turn the json into that object
+	public Recipe create(@RequestBody Recipe recipe) {
+		return recipeRepo.save(recipe);
+	}
 	
 
 }
