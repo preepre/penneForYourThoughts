ea593a6651c60e8d6f5c6112883e329ed3476b85
Priya Prabhakar <Priya.Prabhakar@LibertyMutual.com>
2017-09-07 14:15:44 -0700

    adding delete ingredient round 2

--- src/main/java/com/libertymutual/goforcode/penneForYourThoughts/api/IngredientApiController.java
+++ src/main/java/com/libertymutual/goforcode/penneForYourThoughts/api/IngredientApiController.java
@@ -1,6 +1,5 @@
 package com.libertymutual.goforcode.penneForYourThoughts.api;
 
-
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.libertymutual.goforcode.penneForYourThoughts.models.Ingredient;
 import com.libertymutual.goforcode.penneForYourThoughts.models.Recipe;
 import com.libertymutual.goforcode.penneForYourThoughts.repositories.IngredientRepository;
 import com.libertymutual.goforcode.penneForYourThoughts.repositories.InstructionRepository;
 import com.libertymutual.goforcode.penneForYourThoughts.repositories.RecipeRepository;
-
 
 @RestController
 @RequestMapping("/api/recipes/{id}/ingredients")
@@ -31,24 +31,35 @@ public class IngredientApiController {
 		this.recipeRepo = recipeRepo;
 		this.ingredientRepo = ingredientRepo;
 		this.instructionRepo = instructionRepo;
-		
+
 	}
-	
+
 	@PostMapping("")
 	public Recipe createIngredientForARecipe(@PathVariable long id, @RequestBody Ingredient ingredient) {
 		Recipe recipe = recipeRepo.findOne(id);
 		ingredient.setRecipe(recipe);
-		
+
 		ingredient = ingredientRepo.save(ingredient);
 		recipe.addIngredient(ingredient);
 		return recipe;
 	}
-	
+
 	// Get all ingredients for a recipe
-		@GetMapping("")
-		public List<Ingredient> getAll() {
-			return ingredientRepo.findAll();
+	@GetMapping("")
+	public List<Ingredient> getAll() {
+		return ingredientRepo.findAll();
 
 	}
-	
+
+	@DeleteMapping("{ing_id}")
+	public Ingredient deleteIngredientForARecipe(@PathVariable long ing_id) {
+		try {
+			Ingredient ingredient = ingredientRepo.findOne(ing_id);
+			ingredientRepo.delete(ing_id);
+			return ingredient;
+		} catch (EmptyResultDataAccessException erdae) {
+			return null;
+		}
+	}
+
 }
