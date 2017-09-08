68983ef3fac5a1ee4d96b7a71b5cc7fee2670417
Tania Asim <tania.asim@libertymutual.com>
2017-09-08 10:38:17 -0700

    All unit tests are done.

--- src/test/java/com/liberymutual/goforcode/penneForYourThoughts/models/RecipeModelTests.java
+++ src/test/java/com/liberymutual/goforcode/penneForYourThoughts/models/RecipeModelTests.java
@@ -3,33 +3,71 @@ package com.liberymutual.goforcode.penneForYourThoughts.models;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.meanbean.test.BeanTester;
 
+import com.libertymutual.goforcode.penneForYourThoughts.models.Ingredient;
 import com.libertymutual.goforcode.penneForYourThoughts.models.Instruction;
 import com.libertymutual.goforcode.penneForYourThoughts.models.Recipe;
+import com.libertymutual.goforcode.penneForYourThoughts.repositories.IngredientRepository;
 import com.libertymutual.goforcode.penneForYourThoughts.repositories.InstructionRepository;
 import com.libertymutual.goforcode.penneForYourThoughts.repositories.RecipeRepository;
 
 public class RecipeModelTests {
 	
+	
+	private RecipeRepository recipeRepo;
+	private IngredientRepository ingredientRepo;
+	private InstructionRepository instructionRepo;
+
+	@Before
+	public void setUp() {
+		recipeRepo = mock(RecipeRepository.class);
+		ingredientRepo = mock(IngredientRepository.class);
+		instructionRepo = mock(InstructionRepository.class);
+	}
+	
 	@Test
     public void test_all_gets_and_sets() {
         new BeanTester().testBean(Recipe.class);
+        new BeanTester().testBean(Instruction.class);
+        new BeanTester().testBean(Ingredient.class);
+	}
+	
+	@Test
+    public void test_add_Instruction() {
+		//arrange
+		Instruction ins = new Instruction();
+		ArrayList<Instruction> instructions = new ArrayList<Instruction>();
+		instructions.add(ins);
+		
+		//act
+		ArrayList<Instruction> actualInstructions = new ArrayList<Instruction>();
+		Recipe recipe = new Recipe();
+		recipe.addInstruction(ins);
+		actualInstructions = recipe.getInstructions();
+		
+		//assert
+		assertThat(actualInstructions).contains(ins);
     }
 	
 	@Test
-	public void test_add_Instruction() {
-		
-		Recipe recipe1 = new Recipe();
-		Instruction instruction1 = new Instruction();
-		recipe1.addInstruction(instruction1);		
-		
-		//assertThat(recipe1).hasFieldOrPropertyWithValue("instruction", instruction1)).has
-		
-		
-		
-	}
-
+    public void test_add_Ingredients() {
+		//arrange
+		Ingredient ing = new Ingredient();
+		ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+		ingredients.add(ing);
 	
+		//act
+		ArrayList<Ingredient> actualIngredients = new ArrayList<Ingredient>();
+		Recipe recipe = new Recipe();
+		recipe.addIngredient(ing);
+		actualIngredients = recipe.getIngredients();
+		
+		//assert
+		assertThat(actualIngredients).contains(ing);
+    }
 }
